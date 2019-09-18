adfe6a3baa2187c47a17891b50e89134309ded7e
Antonie Kowal <antoniekowal@gmail.com>
2019-09-17 20:52:58 -0400

    properly validated HTML

--- p1/index.php
+++ p1/index.php
@@ -1,7 +1,8 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
   <head>
     <meta charset="UTF-8">
+    <title>The Pixies</title>
     <style>
     	body {
     		text-align: center;
@@ -17,7 +18,7 @@
   </head>
   <body>
   	<h1>The Pixies</h1>
-  	<img src="/images/pixies.png" id="pixiesImage">
+  	<img src="/images/pixies.png" id="pixiesImage" alt="The Pixies">
   	<h2>Biography</h2>
   	<p>The Pixies are an alternative rock band formed in 1986 in Boston, Massachusetts. The band is comprised of Black Francis on vocals, David Lovering on drums, Joey Santiago on lead guitar, and Paz Lenchantin on bass. Former bassist Kim Deal left the band in 2013, but was a member of the band during the height of their popularity in the late 80's and early 90's. The band has 7 studio albums, 4 of which were released between 1988 and the beginning of their hiatus in 1991. The band reformed in 2004 to play a reunion tour, which was met with much excitement from the fans. The band began writing music again in 2014 after Kim Deal left due to differences in creativity between her and Black Francis.
   </body>
