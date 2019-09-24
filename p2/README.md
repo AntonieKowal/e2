9b0b71c9033cee5a11f656052a4cedcf0e21e224
Antonie Kowal <antoniekowal@gmail.com>
2019-09-24 18:51:47 -0400

    Started p2; PHP practice files

---
+++ p2/README.md
@@ -0,0 +1,9 @@
+# Project 2
++ By: Antonie Kowal
++ Production URL: <http://p2.antoniephp.com>
+
+## Game planning
+
+## Outside resources
+
+## Notes for instructor
---
+++ p2/index.php
@@ -0,0 +1,21 @@
+<?php require "index-controller.php"; ?>
+<!DOCTYPE html>
+<html lang='en'>
+<head>
+
+    <title>Rock Paper Sissors</title>
+    <meta charset='utf-8'>
+
+</head>
+
+<body>
+
+    <h1>Rock Paper Sissors</h1>
+
+    <h2>Mechanics</h2>
+
+    <h2>Results</h2>
+
+</body>
+
+</html>
