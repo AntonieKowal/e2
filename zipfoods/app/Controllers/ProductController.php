e0110162c53bdebb590ef31ebe68d76e827e7f32
Antonie Kowal <antoniekowal@gmail.com>
2019-12-03 12:32:51 -0500

    week 12 database integration

--- zipfoods/app/Controllers/ProductController.php
+++ zipfoods/app/Controllers/ProductController.php
@@ -1,7 +1,8 @@
 <?php
 namespace App\Controllers;
 
-use App\Products;
+# Products class is no longer needed for retrieving the data
+// use App\Products;
 
 class ProductController extends Controller
 {
@@ -10,13 +11,14 @@ class ProductController extends Controller
     public function __construct($app)
     {
         parent::__construct($app);
-        $this->products = new Products($this->app->path("database/products.json"));
     }
 
     public function index()
     {
+        $products = $this->app->db()->all("products");
+
         return $this->app->view("products.index", [
-            'products' => $this->products->getAll()
+            'products' => $products
         ]);
     }
 
@@ -24,15 +26,50 @@ class ProductController extends Controller
     {
         $id = $this->app->param("id");
 
-        $product = $this->products->getById($id);
+        if (is_null($id)) {
+            $this->app->redirect('/products');
+        }
+
+        // $product = $this->products->getById($id);
+        $product = $this->app->db()->findById("products", $id);
 
         if (is_null($product)) {
             return $this->app->view("products.missing", [
             "id" => $id]);
         }
 
+        # load the review details
+        $reviews = $this->app->db()->findByColumn("reviews", "product_id", "=", $id);
+
+        $confirmationName = $this->app->old('confirmationName');
+
         return $this->app->view("products.show", [
-            "product" => $product
+            "product" => $product,
+            "reviews" => $reviews,
+            "confirmationName" => $confirmationName
         ]);
+    }
+
+    public function saveReview()
+    {
+        $this->app->validate([
+            'name' => 'required',
+            'content' => 'required|minLength:200',
+        ]);
+
+        $name = $this->app->input('name');
+        $content = $this->app->input('content');
+        $id = $this->app->input('id');
+
+        # insert into the db
+        $data = [
+            "name" => $name,
+            "content" => $content,
+            "product_id" => $id,
+        ];
+
+        $this->app->db()->insert("reviews", $data);
+
+        $this->app->redirect('/product?id='.$id, ['confirmationName' => $name]);
     }
 }
